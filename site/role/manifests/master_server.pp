ad326e90442e32c064a708d6120d4b6be60f4e3a
koolaji <44560312+koolaji@users.noreply.github.com>
2018-11-05 18:09:55 +0330

    Update master_server.pp

--- site/role/manifests/master_server.pp
+++ site/role/manifests/master_server.pp
@@ -2,4 +2,5 @@ class role::master_server {
   include profile::base
   include profile::agent_nodes
   include pe_repo::platform::ubuntu_1604_amd64
+  include include pe_repo::platform::ubuntu_1604_amd64
 }
