d84abd3db8d66a86f03cf4c9089f4b7b6cd9206a
koolaji <44560312+koolaji@users.noreply.github.com>
2018-11-05 19:02:16 +0330

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -6,7 +6,7 @@ node default {
   }
   user{ 'puppet':
     name       => 'puppet',
-    groups     => 'puppet',
+    #groups     => 'puppet',
     managehome => true,
     password   => '$1$wWXejFYb$jK1ULefOHBEsbYsBXR0.V.',
     ensure     => present
